bdba9d7f2f58446f607eab9920d4970429e45ba8
Thomas Champagne <th.champagne@gmail.com>
2016-04-23 13:56:15 +0200

    #193 Fast display activities synced with trimp in a table

--- hook/extension/options/app/controllers/FitnessController.js
+++ hook/extension/options/app/controllers/FitnessController.js
@@ -4,7 +4,20 @@ app.controller("FitnessController", ['$scope', 'ChromeStorageService', 'Notifier
 
     ChromeStorageService.fetchComputedActivities(function(computedActivities) {
         $scope.computedActivities = computedActivities;
+
+        $scope.namesTrimpsArray = [];
+        _.each(computedActivities, function (activity) {
+            if(activity.extendedStats && activity.extendedStats.heartRateData) {
+                // console.log(activity.name + ' ' + activity.extendedStats.heartRateData.TRIMP);
+                $scope.namesTrimpsArray.push({
+                    date: activity.start_date,
+                    name: activity.name,
+                    trimp: parseInt(activity.extendedStats.heartRateData.TRIMP.toFixed(0))
+                });
+            }
+        });
+
         $scope.$apply();
     });
-    
+
 }]);
