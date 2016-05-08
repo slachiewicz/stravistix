61b3906d203701ee7dc851648ad40c0531f269da
Thomas Champagne <th.champagne@gmail.com>
2016-05-08 19:43:02 +0200

    #193 Init fitness table

--- hook/extension/options/app/controllers/FitnessTrendController.js
+++ hook/extension/options/app/controllers/FitnessTrendController.js
@@ -1,8 +1,3 @@
 app.controller("FitnessTrendController", ['$scope', '$timeout', '$location', 'FitnessDataService', function($scope, $timeout, $location, fitnessDataService) {
 
 }]);
-
-// app.controller("FitnessGraphController", ['$scope', '$timeout', '$location', 'FitnessDataService', function($scope, $timeout, $location, fitnessDataService) {
-//
-//
-// }]);
