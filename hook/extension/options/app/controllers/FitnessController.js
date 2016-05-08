2d023dc983aae111f10ead2bc9eb4567195d2cfd
Thomas Champagne <th.champagne@gmail.com>
2016-05-08 13:50:51 +0200

    #193 create service and refactor fitness

--- hook/extension/options/app/controllers/FitnessController.js
+++ hook/extension/options/app/controllers/FitnessController.js
@@ -1,6 +1,4 @@
-app.controller("FitnessController", ['$scope', 'ChromeStorageService', '$timeout', '$location', function($scope, ChromeStorageService, $timeout, $location) {
-
-    var DAY_LONG_MILLIS = 24 * 3600 * 1000;
+app.controller("FitnessController", ['$scope', '$timeout', '$location', 'FitnessDataService', function($scope, $timeout, $location, fitnessDataService) {
 
     $scope.query = {
         order: 'name',
@@ -34,192 +32,9 @@ app.controller("FitnessController", ['$scope', 'ChromeStorageService', '$timeout
         $scope.updateFitnessChartGraph();
     };
 
-    /**
-     * @return
-     */
-    $scope.getDayAtMidnight = function(date) {
-        date.setHours(Math.abs(date.getTimezoneOffset() / 60));
-        date.setMinutes(0);
-        date.setSeconds(0);
-        date.setMilliseconds(0);
-        return date;
-    };
+    fitnessDataService.getFitnessData().then(function successGet(fitnessData) {
 
-    /**
-     * @return
-     */
-    $scope.filterActivitiesWithHRData = function(computedActivities) {
-        var activitiesWithHRData = [];
-        _.each(computedActivities, function(activity) {
-            if (activity.extendedStats && activity.extendedStats.heartRateData) {
-                var date = $scope.getDayAtMidnight(new Date(activity.start_time));
-                activitiesWithHRData.push({
-                    date: date,
-                    timestamp: date.getTime(),
-                    activityName: activity.name,
-                    trimp: parseInt(activity.extendedStats.heartRateData.TRIMP.toFixed(0))
-                });
-            }
-        });
-        return activitiesWithHRData;
-    };
-
-    /**
-     * @return
-     */
-    $scope.computeCtlAtlTsb = function(everyDaysTrimpArray) {
-        // Now compute
-        var ctl = 0;
-        var atl = 0;
-        var tsb = 0;
-
-        var results = [];
-
-        _.each(everyDaysTrimpArray, function(trimpObject, index, activitiesWithHRData) {
-
-            ctl = ctl + (trimpObject.trimp - ctl) * (1 - Math.exp(-1 / 42));
-            atl = atl + (trimpObject.trimp - atl) * (1 - Math.exp(-1 / 7));
-            tsb = ctl - atl;
-
-            results.push({
-                date: trimpObject.date.toLocaleDateString(),
-                timestamp: trimpObject.timestamp,
-                activitiesName: trimpObject.activitiesName,
-                ctl: ctl.toFixed(3),
-                atl: atl.toFixed(3),
-                tsb: tsb.toFixed(3),
-            });
-
-        });
-
-        return results;
-    };
-
-    /**
-     * @return
-     */
-    $scope.computeRestLooseGain = function(results) {
-
-        // Find the date and loos
-        var lastResult = _.clone(_.last(results));
-
-        var dayCountLostCtl = 1;
-        var dayCountLostAtl = 1;
-        var dayCountInForm = 0;
-        var dayCountLostForm = 0;
-
-        lastResult.ctl = parseInt(lastResult.ctl);
-        lastResult.atl = parseInt(lastResult.atl);
-
-        var ctlLooseTriggerPercentage = 5;
-        var ctlLooseTrigger = ctlLooseTriggerPercentage / 100 * lastResult.ctl;
-
-        var atlLooseTriggerPercentage = 5;
-        var atlLooseTrigger = atlLooseTriggerPercentage / 100 * lastResult.atl;
-
-        while (lastResult.ctl > ctlLooseTrigger) {
-
-            lastResult.ctl = lastResult.ctl + (0 - lastResult.ctl) * (1 - Math.exp(-1 / 42));
-            lastResult.atl = lastResult.atl + (0 - lastResult.atl) * (1 - Math.exp(-1 / 7));
-            lastResult.tsb = lastResult.ctl - lastResult.atl;
-
-            if (lastResult.ctl > ctlLooseTrigger) {
-                dayCountLostCtl++;
-            }
-            if (lastResult.atl > atlLooseTrigger) {
-                dayCountLostAtl++;
-            }
-
-            if (lastResult.tsb <= 0) {
-                dayCountInForm++;
-            } else { // Positive
-                dayCountLostForm++;
-            }
-        }
-
-        return {
-            lostCtl: {
-                percentageTrigger: 100 - ctlLooseTriggerPercentage,
-                dayCount: dayCountLostCtl,
-                date: new Date((new Date().getTime() + dayCountLostCtl * DAY_LONG_MILLIS))
-            },
-            lostAtl: {
-                percentageTrigger: 100 - atlLooseTriggerPercentage,
-                dayCount: dayCountLostAtl,
-                date: new Date((new Date().getTime() + dayCountLostAtl * DAY_LONG_MILLIS))
-            },
-            gainForm: {
-                dayCount: dayCountInForm,
-                date: new Date((new Date().getTime() + dayCountInForm * DAY_LONG_MILLIS))
-            },
-            lostForm: {
-                dayCount: dayCountLostForm,
-                date: new Date((new Date().getTime() + dayCountLostForm * DAY_LONG_MILLIS))
-            },
-        };
-    };
-
-    /**
-     * @return
-     */
-    $scope.prepareTrimpObjectsWithDaysOff = function(activitiesWithHRData) {
-
-        var fromDate = new Date((_.first(activitiesWithHRData)).date);
-
-        var toDate = new Date(); // today
-
-        // Inject day off..
-        var daysDiff = Math.ceil(Math.abs(toDate.getTime() - fromDate.getTime()) / DAY_LONG_MILLIS);
-
-        var everyDaysTrimpArray = [];
-
-        for (var i = 0; i < daysDiff; i++) {
-
-            var timestampOfCurrentDay = fromDate.getTime() + DAY_LONG_MILLIS * i;
-
-            // Seek if current day with have 1 or serveral trimp. then add...
-            var trimpsObjectFoundOnThatDay = _.where(activitiesWithHRData, {
-                timestamp: timestampOfCurrentDay
-            });
-
-            var everyDayTrimpData = {
-                date: new Date(timestampOfCurrentDay),
-                timestamp: timestampOfCurrentDay,
-                activitiesName: [],
-                trimp: 0
-            };
-
-            if (trimpsObjectFoundOnThatDay.length) {
-
-                // Some trimp have beed found for that day
-                if (trimpsObjectFoundOnThatDay.length > 1) {
-                    console.warn('More than 1 activity on ' + trimpsObjectFoundOnThatDay.date + ', handle this on names displayed...');
-                }
-
-                for (var j = 0; j < trimpsObjectFoundOnThatDay.length; j++) {
-                    everyDayTrimpData.trimp += parseFloat(trimpsObjectFoundOnThatDay[j].trimp);
-                    everyDayTrimpData.activitiesName.push(trimpsObjectFoundOnThatDay[j].activityName);
-                }
-            }
-
-            everyDaysTrimpArray.push(everyDayTrimpData);
-        }
-
-        return everyDaysTrimpArray;
-    };
-
-    ChromeStorageService.fetchComputedActivities(function(computedActivities) {
-
-        // Filter only activities with HeartRateData to compute trimp
-        $scope.activitiesWithHRData = $scope.filterActivitiesWithHRData(computedActivities);
-
-        $scope.trimpObjectsWithDaysOff = $scope.prepareTrimpObjectsWithDaysOff($scope.activitiesWithHRData);
-
-        // Generate table & graph data
-        $scope.fitnessData = $scope.computeCtlAtlTsb($scope.trimpObjectsWithDaysOff);
-
-        // Compute rest Loose Gain Data
-        $scope.restLooseGainData = $scope.computeRestLooseGain($scope.fitnessData);
+        $scope.fitnessData = fitnessData;
 
         $scope.updateFitnessChartGraph();
 
@@ -227,9 +42,9 @@ app.controller("FitnessController", ['$scope', 'ChromeStorageService', '$timeout
 
     });
 
-
     $scope.updateFitnessChartGraph = function() {
-        $scope.fitnessChartData = $scope.generateFitnessChartData($scope.fitnessData);
+
+        $scope.fitnessChartData = $scope.generateFitnessGraphData($scope.fitnessData);
 
         $scope.generateGraph();
     };
@@ -378,14 +193,14 @@ app.controller("FitnessController", ['$scope', 'ChromeStorageService', '$timeout
 
     };
 
-    $scope.generateFitnessChartData = function(fitnessData) {
+    $scope.generateFitnessGraphData = function(fitnessData) {
 
         // Compute from timstamp
         var fromTimestamp;
         if ($scope.periodSelected.days === 0) {
             fromTimestamp = (_.first(fitnessData)).timestamp;
         } else {
-            fromTimestamp = new Date().getTime() - $scope.periodSelected.days * DAY_LONG_MILLIS;
+            fromTimestamp = new Date().getTime() - $scope.periodSelected.days * fitnessDataService.static.DAY_LONG_MILLIS;
         }
 
         var ctlValues = [];
